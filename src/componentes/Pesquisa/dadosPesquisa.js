f33e68fe09f8ed09f15494937f30ddd9d959d6ef
charbeldecastro <charbeldecastro@gmail.com>
2025-09-23 22:12:06 -0300

    Estados

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,10 @@
+import livro from '../../imagens/livro.png'
+
+export const Livros = [
+    {nome: 'Liderança em Design', id: 1, src: livro},
+    {nome: 'Liderança em UX', id: 1, src: livro},
+    {nome: 'JavaScript Assertivo', id: 2, src: livro},
+    {nome: 'Cangaceiro em JacaScript', id: 3, src: livro},
+    {nome: 'Aventureiros do JavaScript', id: 41, src: livro},
+
+]
